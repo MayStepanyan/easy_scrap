ff9cfc409e0d16981ab15820f5c8433c6c185ccc
mayis <mayis_s@purlin.com>
2023-01-22 20:13:02 +0400

    Add constants to separate file

---
+++ easy_scrape/constants.py
@@ -0,0 +1,9 @@
+WINDOW_SIZE = (1480, 1200)
+IMG_PER_ROW = 3
+IMG_CLS_NAME = "n3VNCb"
+XPATH_TEMPLATE = '//*[@id="islrg"]/div[1]/div[%s]/a[1]/div[1]/img'
+PAGE_LOAD_TIME = 0.5
+SHOW_MORE_BTN_NAME = "mye4qd"
+MAX_RETRIES_PER_IMAGE = 3
+SEARCH_URL = 'https://www.google.com/search?q='
+MAIN_DIR = "scrapped_images"
